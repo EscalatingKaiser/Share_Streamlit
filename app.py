d4bffdce79c0f3849e8bf4e1628361f075a5a329
EscalatingKaiser <EscalatingKaiser@gmail.com>
2025-04-02 15:00:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Sample Data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+# Create a plot
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+# Streamlit app
+st.title("My Interactive Plot")
+st.pyplot(fig)
